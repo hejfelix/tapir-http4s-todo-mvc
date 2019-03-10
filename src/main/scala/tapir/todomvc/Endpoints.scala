59800d72e40b58d0de5365c525eb77bf0eda79af
Felix Palludan Hargreaves <hejfelix@gmail.com>
2019-03-10 13:24:35 +0100

    Better documentation

--- src/main/scala/tapir/todomvc/Endpoints.scala
+++ src/main/scala/tapir/todomvc/Endpoints.scala
@@ -1,15 +1,15 @@
 package tapir.todomvc
 import java.util.UUID
 
+import cats.implicits._
 import io.circe.generic.auto._
 import tapir.Codec.PlainCodec
-import tapir._
-import tapir.json.circe._
-import cats.implicits._
 import tapir.DecodeResult.{Missing, Value}
+import tapir._
 import tapir.docs.openapi._
+import tapir.json.circe._
+import tapir.openapi.Info
 import tapir.openapi.circe.yaml._
-import tapir.openapi.{Info, OpenAPI}
 
 class Endpoints(val basePath: String) {
 
@@ -20,21 +20,19 @@ class Endpoints(val basePath: String) {
       )(_.toString)
 
   private lazy val info = Info(
-    "TodoMVC-Backend",
+    "TodoMVC-Backend using Tapir",
     "1.0",
   )
 
-  private def allDocs: OpenAPI =
+  def openApiYaml: String =
     List(
-      deleteEndpoint.toOpenAPI(info),
-      getEndpoint.toOpenAPI(info),
-      getTodoEndpoint.toOpenAPI(info),
-      patchByIdEndpoint.toOpenAPI(info),
-      deleteTodoEndpoint.toOpenAPI(info)
-    ).foldLeft(postEndpoint.toOpenAPI(info))((api, api2) =>
-      api2.paths.foldLeft(api)((api, path) => api.addPathItem(path._1, path._2)))
-
-  def openApiYaml: String = allDocs.toYaml
+      deleteEndpoint,
+      getEndpoint,
+      getTodoEndpoint,
+      patchByIdEndpoint,
+      deleteTodoEndpoint,
+      postEndpoint
+    ).toOpenAPI(info).toYaml
 
   lazy val postEndpoint: Endpoint[Todo, String, Todo, Nothing] =
     endpoint.post
@@ -42,30 +40,35 @@ class Endpoints(val basePath: String) {
       .in(jsonBody[Todo])
       .out(jsonBody[Todo])
       .errorOut(stringBody)
+      .description("Create a new TODO item")
 
   lazy val deleteTodoEndpoint: Endpoint[UUID, String, List[Todo], Nothing] =
     endpoint.delete
       .in(basePath / path[UUID]("uuid"))
       .out(jsonBody[List[Todo]])
       .errorOut(stringBody)
+      .description("Delete a TODO item using its UUID")
 
   lazy val deleteEndpoint: Endpoint[Unit, String, List[Todo], Nothing] =
     endpoint.delete
       .in(basePath)
       .out(jsonBody[List[Todo]])
       .errorOut(stringBody)
+      .description("Delete all todos")
 
   lazy val getEndpoint: Endpoint[Unit, String, List[Todo], Nothing] =
     endpoint.get
       .in(basePath)
       .out(jsonBody[List[Todo]])
       .errorOut(stringBody)
+      .description("Retrieve all TODOs")
 
   lazy val getTodoEndpoint: Endpoint[UUID, String, Todo, Nothing] =
     endpoint.get
       .in(basePath / path[UUID]("uuid"))
       .out(jsonBody[Todo])
       .errorOut(stringBody)
+      .description("Get TODO item by UUID")
 
   lazy val patchByIdEndpoint: Endpoint[(UUID, Todo), String, Todo, Nothing] =
     endpoint.patch
@@ -73,5 +76,6 @@ class Endpoints(val basePath: String) {
       .in(jsonBody[Todo])
       .out(jsonBody[Todo])
       .errorOut(stringBody)
+      .description("Patch TODO item by UUID")
 
 }
